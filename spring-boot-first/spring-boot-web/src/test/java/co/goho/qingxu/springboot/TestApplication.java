aa10d9a26c8d650ddca77815752b97637d16b74c
gwj342643974 <342643974@qq.com>
2019-10-15 16:53:17 +0800

    1

---
+++ spring-boot-first/spring-boot-web/src/test/java/co/goho/qingxu/springboot/TestApplication.java
@@ -0,0 +1,15 @@
+package co.goho.qingxu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
